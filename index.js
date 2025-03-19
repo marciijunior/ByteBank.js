5ca558c55b49b9af6494a74de990775c3187ef84
marciijunior <marciojsjunior.mj@gmail.com>
2025-03-19 18:41:12 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const cliente1Nome = 'Ricardo';
+const cliente1CPF = '123456789';
+const cliente1Agencia = '0001';
+const cliente1Saldo = '0';
+
+const cliente2Nome = 'Glauber';
+const cliente2CPF = '123456789';
+const cliente2Agencia = '0001';
+const cliente2Saldo = '0';
+
+const cliente3Nome = 'Rafael';
+const cliente3CPF = '123456789';
+const cliente3Agencia = '0001';
+const cliente3Saldo = '0';
+
+console.log(cliente1Nome);
