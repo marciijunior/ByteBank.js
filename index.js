5b38e2daa1d4deee81587cc024aee1c98a0decc9
marciijunior <marciojsjunior.mj@gmail.com>
2025-03-28 12:45:33 -0300

    Tentativa de debug

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Cliente } from "./cliente.js";
+import { ContaCorrente } from "./contaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+// Criando clientes
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+// Criando contas correntes
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2);
 
 let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
+contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log("valor: ", valor)
-console.log(conta2);
+// Exibindo os objetos no console
 console.log(contaCorrenteRicardo);
+console.log(conta2);
